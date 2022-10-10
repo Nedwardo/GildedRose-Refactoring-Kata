9612bfb6817106832889dcc04adb97e37a419e72
root <root@LPT-J8NQ5J3.localdomain>
2022-10-10 13:35:32 +0100

    Refactored code
    Signed-off-by: root <root@LPT-J8NQ5J3.localdomain>

--- Java/src/main/java/com/gildedrose/GildedRoseItem.java
+++ Java/src/main/java/com/gildedrose/GildedRoseItem.java
@@ -1,17 +1,16 @@
 package com.gildedrose;
 
-public class GildedRoseItems {
-    Item item;
-    QualityChange qualityChange;
-    public GildedRoseItems(String name, int sellIn, int quality, QualityChange qualityChange) {
-        item = new Item (name, sellIn, quality);
+public class GildedRoseItem {
+    private final Item item;
+    private final QualityChange qualityChange;
+    public GildedRoseItem(Item item, QualityChange qualityChange) {
+        this.item = item;
         this.qualityChange = qualityChange;
     }
-
     public void updateQuality(){
-        item.quality = qualityChange.updateQuality(item.quality);
+        item.quality = qualityChange.updateQuality(item.sellIn, item.quality);
+        item.sellIn = qualityChange.updateSellIn(item.sellIn);
     }
-
     @Override
     public String toString() {
         return item.name + ", " + item.sellIn + ", " + item.quality;
--- Java/src/main/java/com/gildedrose/GildedRoseItemFactory.java
+++ Java/src/main/java/com/gildedrose/GildedRoseItemFactory.java
@@ -1,2 +1,18 @@
-package com.gildedrose;public class GildedRoseItemFactory {
+package com.gildedrose;
+
+public class GildedRoseItemFactory {
+    QualityChange[] qualityChanges;
+    public GildedRoseItemFactory(QualityChange[] qualityChanges){
+        this.qualityChanges = qualityChanges;
+    }
+    public GildedRoseItem CreateInstance(Item item){
+        return new GildedRoseItem(item, assignQualityChange(item));
+    }
+    private QualityChange assignQualityChange(Item item) {
+        for (QualityChange qualityChange : qualityChanges){
+            if (qualityChange.nameMatchesThisType(item.name))
+                return qualityChange;
+        }
+        return new DefaultQualityChange();
+    }
 }
